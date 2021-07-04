c9ca1c95f16bd4c386f3f0a36d124a61297a5b9d
Sergey Karpik <sergey.karpik@psa.inc>
2021-07-04 22:29:32 +0300

    solved task 1

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,33 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val posters = array[0]
+        val colorVariants = array[1].toLong()
+        var res: Int? = null
+
+        for (i in 1..colorVariants)
+        {
+            if(findMatches(colorVariants, i) == posters)
+            {
+                res = i.toInt()
+                break
+            }
+        }
+
+        return res
+    }
+
+    fun findMatches(n: Long, k: Long):Int
+    {
+        return (factorial(n)/(factorial(n-k) * factorial(k))).toInt()
+    }
+
+    fun factorial(value: Long): Long {
+        return if(value == 0L) {
+            1
+        } else {
+            value * factorial(value-1)
+        }
     }
 }
