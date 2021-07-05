3d09663edb30ec4df7a6d2067f9b9d0a77087ec4
Sergey Karpik <sergey.karpik@psa.inc>
2021-07-05 21:41:14 +0300

    solved task 2 and 3

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,41 @@
 package subtask2
 
+import java.util.ArrayList
+
+
+
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
-    fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+    fun decomposeNumber(n: Long): Array<Int>? {
+        val decomposeArray = Decomposer(n, n * n) ?: return null
+
+        decomposeArray.removeAt(decomposeArray.size - 1)
+        var result = Array<Int>(decomposeArray.size){0}
+        for (i in 0..decomposeArray.size - 1) {
+            result[i] = decomposeArray[i].toInt()
+        }
+        return result
+    }
+
+    fun Decomposer(n: Long, remain: Long): MutableList<Long>? {
+        if (remain == 0L) {
+            val r: MutableList<Long> = ArrayList()
+            r.add(n)
+            return r
+        }
+
+        for (i in n - 1 downTo 1) {
+            if (remain - i * i >= 0) {
+                val r = Decomposer(i, remain - i * i)
+
+                if (r != null) {
+                    r.add(n)
+                    return r
+                }
+            }
+        }
+
+        return null
     }
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -2,8 +2,59 @@ package subtask3
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
-    fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+    fun findAllNumbersFromGivenNumber(number: String): Array<String>?{
+
+        var res: ArrayList<String> = ArrayList()
+
+        for (i in number.indices) {
+            try {
+                res.addAll(getAllNear(i, number))
+            }
+            catch (e: Exception)
+            {
+                return null
+            }
+        }
+
+        var resArray: Array<String> = Array(res.size) { "empty" }
+        for (i in res.indices) {
+            resArray[i] = res[i]
+        }
+
+
+        return resArray
     }
+
+    fun getAllNear(indx: Int, input: String): ArrayList<String> {
+        val getNearTo = input[indx].toString().toInt()
+        val currentArray = numberNeighbors.get(getNearTo)
+        var result = ArrayList<String>()
+
+        if (currentArray != null) {
+            for (i in 0..currentArray.size-1) {
+                var stringToAdd: String = ""
+                stringToAdd += input.substring(0, indx)
+                stringToAdd += currentArray[i]
+                if(indx < input.length) {
+                    stringToAdd += input.substring(indx + 1)
+                }
+                result.add(stringToAdd)
+            }
+        }
+
+        return result
+    }
+
+    var numberNeighbors = mapOf<Int, Array<Int>>(
+        0 to arrayOf(8),
+        1 to arrayOf(2,4),
+        2 to arrayOf(1,3,5),
+        3 to arrayOf(2,6),
+        4 to arrayOf(1,5,7),
+        5 to arrayOf(2,4,6,8),
+        6 to arrayOf(3,5,9),
+        7 to arrayOf(4,8),
+        8 to arrayOf(5,7,9,0),
+        9 to arrayOf(6,8)
+    )
 }
